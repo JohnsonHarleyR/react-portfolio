eba897ad98d95ee3fe57da759c28ead28d0cca0c
Harley Johnson <62576957+JohnsonHarleyR@users.noreply.github.com>
2025-04-28 00:52:32 +0000

    Got the main menu and all of the displays in the correct positions for both desktop and mobile or tablet.

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Contact1 from './pages/Contact/Contact1';
 import Contact2 from './pages/Contact/Contact2';
 import Contact3 from './pages/Contact/Contact3';
 import { useContext } from 'react';
+import { Button, Input } from '@chakra-ui/react'
 
 function App() {
 
@@ -25,56 +26,11 @@ function App() {
     return classN;
   }
 
-  return (
-      <Router>
-        <header>
-          <nav>
-            <div className='logo-area-secondary'>
-              <p>Logo area test</p>
-            </div>
-            <ul>
-              <li>
-                <div className={getClass('Home')} onClick={() => {changeClass("Home")}}>
-                  <Link to="/"><p>Home</p></Link>
-                </div>
-              </li>
-              <li>
-                <div className={getClass('Projects')} onClick={() => {changeClass("Projects")}}>
-                  <Link to="/projects"><p>Projects</p></Link>
-                </div>
-              </li>
-                <div className={getClass('Contact')} onClick={() => {changeClass("Contact")}}>
-                  <Link to="/contact"><p>Contact</p></Link>
-                </div>
-              <li>
-                <div className={getClass('GitHub')}>
-                  <p><a href="https://github.com/JohnsonHarleyR" target="_blank">GitHub</a></p>
-                </div>
-              </li><li>
-                <div className={getClass('LinkedIn')}>
-                <p><a href="https://www.linkedin.com/in/johnsonharleyr" target="_blank">LinkedIn</a></p>
-                </div>
-              </li><li>
-                <div className={getClass('Resume')}>
-                <p><a href="src/assets/resume.pdf" target="_blank">Resume</a></p>
-                </div>
-              </li><li>
-
-              </li>
-            </ul>
-          </nav>
-        </header>
-        {/* Main area under the header on desktop */}
-        <div className='container1'>
-          <Routes>
-            <Route path="/" element={<Home1 />} />
-            <Route path="/projects" element={<Projects1 />} />
-            <Route path="/contact" element={<Contact1 />} />
-          </Routes>
-        </div>
-        {/* Cerulean area on the left side for desktop */}
-        <div className='container2'>
-          <div className='logo-area-primary'>
+  function getContainer2(areaType = "primary") {
+    console.log('getContainer2 areaType: ', areaType);
+    return (
+      <div className={`container2 ${areaType}`}>
+          <div className={`logo-area primary`}>
             <p>Logo area test</p>
           </div>
           <Routes>
@@ -82,16 +38,81 @@ function App() {
             <Route path="/projects" element={<Projects2 />} />
             <Route path="/contact" element={<Contact2 />} />
           </Routes>
-        </div>
-        {/* Light blue area below main area */}
-        <div className='container3'>
-          <Routes>
-            <Route path="/" element={<Home3 />} />
-            <Route path="/projects" element={<Projects3 />} />
-            <Route path="/contact" element={<Contact3 />} />
-          </Routes>
-        </div>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      <Router>
+        {/* Cerulean area on the left side for desktop */}
+        {getContainer2('primary')}
+        <section className="right-area">
+          <div className="container1">
+            <header>
+            <div className='logo-area secondary'>
+                      <p>Logo area test</p>
+                    </div>
+              <nav>
+                <ul>
+                  <li>
+                    <div className={getClass('Home')} onClick={() => {changeClass("Home")}}>
+                      <Link to="/"><p>Home</p></Link>
+                    </div>
+                  </li>
+                  <li>
+                    <div className={getClass('Projects')} onClick={() => {changeClass("Projects")}}>
+                      <Link to="/projects"><p>Projects</p></Link>
+                    </div>
+                  </li>
+                    <div className={getClass('Contact')} onClick={() => {changeClass("Contact")}}>
+                      <Link to="/contact"><p>Contact</p></Link>
+                    </div>
+                  <li>
+                    <div className={getClass('GitHub')}>
+                      <p><a href="https://github.com/JohnsonHarleyR" target="_blank">GitHub</a></p>
+                    </div>
+                  </li><li>
+                    <div className={getClass('LinkedIn')}>
+                    <p><a href="https://www.linkedin.com/in/johnsonharleyr" target="_blank">LinkedIn</a></p>
+                    </div>
+                  </li><li>
+                    <div className={getClass('Resume')}>
+                    <p><a href="src/assets/resume.pdf" target="_blank">Resume</a></p>
+                    </div>
+                  </li>
+                </ul>
+              </nav>
+            </header>
+            {/* Put container2 here if the screen is a portrait or on mobile */}
+            {getContainer2('secondary')}
+            {/* Main area under the header on desktop */}
+            <div>
+              <Routes>
+                <Route path="/" element={<Home1 />} />
+                <Route path="/projects" element={<Projects1 />} />
+                <Route path="/contact" element={<Contact1 />} />
+              </Routes>
+            </div>
+          </div>
+          
+          
+          {/* Light blue area below main area */}
+          <div className='container3'>
+            <Button colorScheme={"blue"}>Chakra Button</Button>
+            <input placeholder="test" />
+            <Input placeholder="test"/>
+            <Routes>
+              <Route path="/" element={<Home3 />} />
+              <Route path="/projects" element={<Projects3 />} />
+              <Route path="/contact" element={<Contact3 />} />
+            </Routes>
+          </div>
+
+        </section>
+        
       </Router>
+    </main>
   )
 }
 
